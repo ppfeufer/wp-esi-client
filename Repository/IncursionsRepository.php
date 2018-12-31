3c757ec746ea57d9863e11784e86fb457a497ff6
Peter Pfeufer <ppfeufer@gmail.com>
2018-12-31 16:20:13 +0100

    Added Incursions

---
+++ Repository/IncursionsRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * Copyright (C) 2018 ppfeufer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace WordPress\EsiClient\Repository;
+
+use WordPress\EsiClient\Model\Incursions\Incursions;
+use WordPress\EsiClient\Swagger;
+
+\defined('ABSPATH') or die();
+
+class IncursionsRepository extends Swagger {
+    /**
+     * Used ESI enpoints in this class
+     *
+     * @var array ESI enpoints
+     */
+    protected $esiEndpoints = [
+        'incursions' => 'incursions/?datasource=tranquility'
+    ];
+
+    /**
+     * Shows sovereignty data for campaigns
+     *
+     * @return Incursions
+     */
+    public function incursions() {
+        $returnValue = null;
+
+        $this->setEsiMethod('get');
+        $this->setEsiRoute($this->esiEndpoints['incursions']);
+        $this->setEsiVersion('v1');
+
+        $esiData = $this->callEsi();
+
+        if(!\is_null($esiData)) {
+            $returnValue = $this->map($esiData, new Incursions);
+        }
+
+        return $returnValue;
+    }
+}
